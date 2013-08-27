0325fd36b31aa9ea49062080f03978790c827b41
Callum Stott <oetzi101@gmail.com>
2013-08-26 23:20:24 -0700

    Clean class :rage1:

--- birthday.rb
+++ birthday.rb
@@ -1,13 +1,15 @@
 require 'sinatra'
 
 class Birthday < Sinatra::Base
-  def birthday?(date)
-    date.month == 8 && date.day == 27
-  end
-
   get'/' do
     erb :index, :locals => {
       :birthday => birthday?(Date.today)
     }
+  end
+
+  private
+
+  def birthday?(date)
+    date.month == 8 && date.day == 27
   end
 end
